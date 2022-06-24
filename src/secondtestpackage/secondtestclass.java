dd517453435b76ed376e4bed2b8e8eed1e3745a3
Triple Baconator <triple@baconator.on.ca>
2022-06-23 21:01:41 -0400

    second test

---
+++ src/secondtestpackage/secondtestclass.java
@@ -0,0 +1,11 @@
+package secondtestpackage;
+
+public class secondtestclass {
+
+	public static void main(String[] args) {
+		System.out.println("this is second test");
+		// TODO Auto-generated method stub
+
+	}
+
+}
