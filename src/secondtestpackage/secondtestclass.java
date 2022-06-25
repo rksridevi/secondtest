6de9aebaf70fa06c85629a8c1b43ef41eb737ff3
Triple Baconator <triple@baconator.on.ca>
2022-06-25 16:05:34 -0400

    second change

--- src/secondtestpackage/secondtestclass.java
+++ src/secondtestpackage/secondtestclass.java
@@ -4,6 +4,7 @@ public class secondtestclass {
 
 	public static void main(String[] args) {
 		System.out.println("this is second test");
+		System.out.println("this is secon change");
 		// TODO Auto-generated method stub
 
 	}
